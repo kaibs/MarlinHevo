d108925cc730bc7343baad53d5768a8f804d2553
kaibs <kai.b.schneider@gmail.com>
2020-04-06 21:55:42 +0200

    fixed M425

--- Marlin-2.0.x/Marlin/Configuration_adv.h
+++ Marlin-2.0.x/Marlin/Configuration_adv.h
@@ -786,7 +786,7 @@
   // Define values for backlash distance and correction.
   // If BACKLASH_GCODE is enabled these values are the defaults.
   #define BACKLASH_DISTANCE_MM { 0, 0, 0 } // (mm)
-  #define BACKLASH_CORRECTION    0.0       // 0.0 = no correction; 1.0 = full correction
+  #define BACKLASH_CORRECTION    0.5       // 0.0 = no correction; 1.0 = full correction
 
   // Set BACKLASH_SMOOTHING_MM to spread backlash correction over multiple segments
   // to reduce print artifacts. (Enabling this is costly in memory and computation!)
