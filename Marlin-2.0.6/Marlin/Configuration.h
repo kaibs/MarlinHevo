395f522e8fcfaa958d7b8bff68e4190d7b1b8106
kaibs <kai.b.schneider@gmail.com>
2020-08-02 23:55:53 +0200

    increased probing margin

--- Marlin-2.0.6/Marlin/Configuration.h
+++ Marlin-2.0.6/Marlin/Configuration.h
@@ -989,7 +989,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 25
+#define PROBING_MARGIN 45
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 2000
@@ -1327,7 +1327,7 @@
   //=================================== Mesh ==================================
   //===========================================================================
 
-  #define MESH_INSET 10          // Set Mesh bounds as an inset region of the bed
+  #define MESH_INSET 30          // Set Mesh bounds as an inset region of the bed
   #define GRID_MAX_POINTS_X 3    // Don't use more than 7 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
